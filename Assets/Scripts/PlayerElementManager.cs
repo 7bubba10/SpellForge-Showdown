3108e9dccf747c2d939e9a81ead8afeeea4182f7
mikea-1415 <michaelalboher@gmail.com>
2025-11-15 14:01:20 -0500

    implemented spell pickup and spell manager scripts

---
+++ Assets/Scripts/PlayerElementManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public enum ElementType
+{
+    None,
+    Fire,
+    Earth,
+    Air,
+    Water
+}
+
+public class PlayerElementManager : MonoBehaviour
+{
+    [Header("Current Element")]
+    public ElementType currentElement = ElementType.None;
+
+    [Header("Element Weapons (assign in Inspector)")]
+    public GameObject fireWeapon;
+    public GameObject earthWeapon;
+    public GameObject airWeapon;
+    public GameObject waterWeapon;
+
+    private void Start()
+    {
+        // Make sure only the correct weapon is active at start
+        EquipElement(currentElement);
+    }
+
+    public void EquipElement(ElementType newElement)
+    {
+        currentElement = newElement;
+
+        // Disable all weapons first
+        if (fireWeapon)  fireWeapon.SetActive(false);
+        if (earthWeapon) earthWeapon.SetActive(false);
+        if (airWeapon)   airWeapon.SetActive(false);
+        if (waterWeapon) waterWeapon.SetActive(false);
+
+        // Enable the selected one
+        switch (currentElement)
+        {
+            case ElementType.Fire:
+                if (fireWeapon) fireWeapon.SetActive(true);
+                break;
+
+            case ElementType.Earth:
+                if (earthWeapon) earthWeapon.SetActive(true);
+                break;
+
+            case ElementType.Air:
+                if (airWeapon) airWeapon.SetActive(true);
+                break;
+
+            case ElementType.Water:
+                if (waterWeapon) waterWeapon.SetActive(true);
+                break;
+
+            case ElementType.None:
+            default:
+                // No weapon
+                break;
+        }
+
+        Debug.Log($"Equipped element: {currentElement}");
+    }
+}
