dba8720340b62fe9280f21704dbdb658a14c244f
Jonathan-Strat <jonathan.strathausen@marist.edu>
2025-11-16 17:05:51 -0500

    Added a 3D scoring bar which acts like a HUD - attached to camera

--- Assets/Scripts/LoadingBarStraight.cs
+++ Assets/Scripts/LoadingBarStraight.cs
@@ -6,49 +6,34 @@ namespace Modularify.LoadingBars3D
 {
     public class LoadingBarStraight : MonoBehaviour
     {
-        #region variables
+        public static LoadingBarStraight Instance;
+
         [Header("Inner Part Reference")]
-        [SerializeField]
-        private GameObject _innerPart;
+        [SerializeField] private GameObject _innerPart;
 
         private Material _innerPartMaterial;
 
         [Header("Loading Bar Settings and Colors")]
         [Range(0.0f, 1.0f)]
-        [SerializeField]
-        private float _percentage = 0.5f;
+        [SerializeField] private float _percentage = 0.5f;
 
         [ColorUsage(true, true)]
-        [SerializeField]
-        private Color _emptyColor = new Color(1, 0, 0, 1);
+        [SerializeField] private Color _emptyColor = Color.red;
 
         [ColorUsage(true, true)]
-        [SerializeField]
-        private Color _fullColor = new Color(0, 1, 0, 1);
-        #endregion
-
-        #region methods
+        [SerializeField] private Color _fullColor = Color.green;
 
         private void Awake()
         {
+            Instance = this;
             Initialize();
         }
-        // Start is called before the first frame update
-        void Start()
-        {
 
-        }
-
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
             _innerPartMaterial.SetFloat("Percentage", _percentage);
         }
 
-        /// <summary>
-        /// Sets the percentage of the progress bar
-        /// </summary>
-        /// <param name="percentage">float value clamped between 0 and 1</param>
         public void SetPercentage(float percentage)
         {
             _percentage = Mathf.Clamp01(percentage);
@@ -58,11 +43,10 @@ namespace Modularify.LoadingBars3D
         {
             _innerPartMaterial = new Material(_innerPart.GetComponent<MeshRenderer>().sharedMaterial);
             _innerPart.GetComponent<MeshRenderer>().material = _innerPartMaterial;
+
             _innerPartMaterial.SetFloat("Percentage", _percentage);
             _innerPartMaterial.SetColor("SideColorEmpty", _emptyColor);
             _innerPartMaterial.SetColor("SideColorFull", _fullColor);
         }
-        #endregion
     }
 }
-
